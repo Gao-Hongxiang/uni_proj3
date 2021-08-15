b7af6d9b43ac4f8ef3d3e73226c4d0c99d837e3e
GHX <2460013249@qq.com>
2021-08-15 11:56:31 +0800

    完成了首页

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import App from './App'
-
+import{$http} from '@escook/request-miniprogram'
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+ uni.showToast({
+  title,
+  duration,
+  icon: 'none',
+})
+}
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
 
+$http.beforeRequest = function(option) {
+  uni.showLoading({
+    
+    title:'数据加载中'
+  })
+}
+$http.afterRequset = function(option) {
+  uni.hideLoading()
+}
+// $http.afterRequest = function(){
+//   uni.hideLoading()
+// }
 const app = new Vue({
 	...App
 })
